e9a14eb30df4513e7a50fab1a301bb45db4301cd
kenanhancer <kenanhancer@gmail.com>
2023-02-07 23:56:02 +0000

    ok

--- src/decorator/app2.ts
+++ src/decorator/app2.ts
@@ -1,30 +1,15 @@
-function loggedMethod(headMessage = "LOG:") {
-  return function actualDecorator(originalMethod: any, context: ClassMethodDecoratorContext) {
-      const methodName = String(context.name);
+type ClassMethodDecorator = <This extends Record<string, any>, Args extends any[], Return>(originalMethod: (this: This, ...args: Args) => Return, context: ClassMethodDecoratorContext<This, typeof originalMethod>) => typeof originalMethod | void;
 
-      function replacementMethod(this: any, ...args: any[]) {
-          console.log(`${headMessage} Entering method '${methodName}'.`)
-          const result = originalMethod.call(this, ...args);
-          console.log(`${headMessage} Exiting method '${methodName}'.`)
-          return result;
-      }
-
-      
-      return replacementMethod;
-  }
-}
-
-function loggedMethod2(headMessage = "LOG:"){
+function loggedMethod(headMessage = "LOG:"){
   return function actualDecorator<This, Args extends any[], Return>(
-    target: (this: This, ...args: Args) => Return,
+    originalMethod: (this: This, ...args: Args) => Return,
     context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>
   ) {
     const methodName = String(context.name);
 
-
     function replacementMethod(this: This, ...args: Args): Return {
         console.log(`LOG: Entering method '${methodName}'.`)
-        const result = target.call(this, ...args);
+        const result = originalMethod.call(this, ...args);
         console.log(`LOG: Exiting method '${methodName}'.`)
         return result;
     }
@@ -34,23 +19,40 @@ function loggedMethod2(headMessage = "LOG:"){
 }
 
 
-// function bound<This, Args extends any[], Return>(originalMethod: (this: This, ...args: Args) => Return, context: ClassMethodDecoratorContext<This, (this: This, ...args: Args) => Return>) {
-//   const methodName = context.name;
-//   if (context.private) {
-//       throw new Error(`'bound' cannot decorate private properties like ${methodName as string}.`);
-//   }
-//   context.addInitializer(function (this) {
-//       this[methodName] = this[methodName].bind(this);
-//   });
-// }
+const bound: ClassMethodDecorator = (originalMethod, context) =>{
+  type This = ThisParameterType<typeof originalMethod>;
 
+  const methodName = context.name as keyof This;
+  
+  if (context.private) {
+      throw new Error(`'bound' cannot decorate private properties like ${methodName as string}.`);
+  }
 
+  context.addInitializer(function (this) {
+      this[methodName] = this[methodName].bind(this);
+  });
+};
+
+const trace: ClassMethodDecorator = (originalMethod, context)=>{
+
+  const methodName = String(context.name);
+
+  return function traceDecorator(this, ...args){
+
+    console.log(`CALL ${methodName}: ${JSON.stringify(args)}`);
+
+    const result = originalMethod.call(this, ...args);
+
+    console.log(`=> ${JSON.stringify(result)}`);
+
+    return result;
+  }
+};
 
 type HasNames = { names: readonly string[] };
 function getNamesExactly<const T extends HasNames>(arg: T): T["names"] {
     return arg.names;
 }
-
 
 
 class Person {
@@ -59,8 +61,9 @@ class Person {
     this.name = name;
   }
 
-  // @bound
-  @loggedMethod2()
+  @trace
+  @bound
+  @loggedMethod()
   greet() {
     console.log(`Hello, my name is ${this.name}.`);
   }
